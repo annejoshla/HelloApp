7eea86334a1d19f499b4cb4904d3f6f81582ba11
Anne <unicornjoshla@gmail.com>
2026-03-18 15:07:26 +0530

    Use case 2

--- HelloApp.java
+++ HelloApp.java
@@ -3,6 +3,11 @@ package packagecom.srm.HelloApp;
 public class HelloApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		String name = args[0];
+
+		System.out.println(name);
+
 	}
 
 }
